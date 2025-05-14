0c53c8279f97512fb58afbff72e36fae6fa25398
MartTartFart <mtsitrinel@gmail.com>
2025-05-14 19:59:08 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,25 +15,24 @@ intents.message_content = True
 # Create bot instance
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Bot startup message
+# Bot ready event
 @bot.event
-async def help():
-    print(f"Commands List:\n\t!assult\n\t!nuke\n\t!ezra\n\t!text\n\t!Jdog")
-    
 async def on_ready():
     print(f"✅ Logged in as {bot.user}")
+    print("Commands List:\n\t!assault\n\t!nuke\n\t!ezra\n\t!text\n\t!Jdog\n\t!askPackgod")
 
-# !assult command - tag someone 40 times (admin only)
+# !assault command - tag someone 40 times (admin only)
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def assault(ctx, user: discord.Member, *, message='assaulted'):
-        for i in range(40):
-            await ctx.send(f"{user.mention} {message}")
-            await asyncio.sleep(1)  # Wait 1 second between messages
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send(f"{user.mention} you do NOT have perms lil bro pack it up")
-    # Do something, like send a message to the user
+    for i in range(40):
+        await ctx.send(f"{user.mention} {message}")
+        await asyncio.sleep(1)
 
+@assault.error
+async def assault_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(f"{ctx.author.mention}, you do NOT have permission to use this command.")
 
 # !nuke command - @everyone spam (admin only)
 @bot.command()
@@ -41,17 +40,24 @@ async def assault(ctx, user: discord.Member, *, message='assaulted'):
 async def nuke(ctx, *, message='boom type shit'):
     for i in range(15):
         await ctx.send(f"@everyone {message}")
-        await asyncio.sleep(1)  # You forgot to "await" before asyncio.sleep()
+        await asyncio.sleep(1)
+
+@nuke.error
+async def nuke_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
-        await ctx.send(f"{user.mention} you do NOT have perms lil bro pack it up")
-# Image commands
+        await ctx.send(f"{ctx.author.mention}, you do NOT have permission to use this command.")
+
+# !ezra - send image
 @bot.command()
 async def ezra(ctx):
     await ctx.send(file=discord.File("ezrah.png"))
 
+# !Jdog - send image
 @bot.command()
 async def Jdog(ctx):
-    await ctx.send(file=discord.File("j-dog.jpg"))  # You wrote .png but posted .jpg
+    await ctx.send(file=discord.File("j-dog.jpg"))
+
+# !text - DM a user (admin only)
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def text(ctx, user: discord.Member, *, message):
@@ -61,13 +67,19 @@ async def text(ctx, user: discord.Member, *, message):
     except discord.Forbidden:
         await ctx.send("❌ I can't send a DM to this user. They might have DMs disabled.")
 
+# !askPackgod - random roast-style 8ball
 @bot.command()
-async def askPackgod(ctx,message):
-    if not message:
+async def askPackgod(ctx, *, message):
+    if not message.strip():
         await ctx.send("you need to ask a question bru")
     else:
-        responses = ['yes','no','nah','FUT NO','dumbass question, sybau','js kys','100%','ask me some supid shit like that again I dare you','im gonna assult you for asking that', 'yes','no','mmmmmaybe','idk bro im packgod']
+        responses = [
+            'yes', 'no', 'nah', 'FUT NO', 'dumbass question, sybau',
+            'js kys', '100%', 'ask me some stupid shit like that again I dare you',
+            'im gonna assault you for asking that', 'mmmmmaybe',
+            'idk bro im packgod'
+        ]
         await ctx.send(random.choice(responses))
-    
-# Start the bot using the token from .env or environment variable
+
+# Run the bot using your token
 bot.run(os.environ['TOKEN'])
