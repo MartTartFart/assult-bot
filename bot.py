ca18349e548336eff23873042fbb35991a09fb22
MartTartFart <mtsitrinel@gmail.com>
2025-05-14 19:50:26 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import asyncio
 import os
+import random
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -25,10 +26,14 @@ async def on_ready():
 # !assult command - tag someone 40 times (admin only)
 @bot.command()
 @commands.has_permissions(administrator=True)
-async def assult(ctx, user: discord.Member, *, message='assulted'):
-    for i in range(40):
-        await ctx.send(f"{user.mention} {message}")
-        await asyncio.sleep(1)  # Wait 1 second between messages
+async def assault(ctx, user: discord.Member, *, message='assaulted'):
+        for i in range(40):
+            await ctx.send(f"{user.mention} {message}")
+            await asyncio.sleep(1)  # Wait 1 second between messages
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(f"{user.mention} you do NOT have perms lil bro pack it up")
+    # Do something, like send a message to the user
+
 
 # !nuke command - @everyone spam (admin only)
 @bot.command()
@@ -37,7 +42,8 @@ async def nuke(ctx, *, message='boom type shit'):
     for i in range(15):
         await ctx.send(f"@everyone {message}")
         await asyncio.sleep(1)  # You forgot to "await" before asyncio.sleep()
-
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(f"{user.mention} you do NOT have perms lil bro pack it up")
 # Image commands
 @bot.command()
 async def ezra(ctx):
@@ -55,5 +61,13 @@ async def text(ctx, user: discord.Member, *, message):
     except discord.Forbidden:
         await ctx.send("❌ I can't send a DM to this user. They might have DMs disabled.")
 
+@bot.command()
+async def askPackgod(ctx,message):
+    if not message:
+        await ctx.send("you need to ask a question bru")
+    else:
+        responses = ['yes','no','nah','FUT NO','dumbass question, sybau','js kys','100%','ask me some supid shit like that again I dare you','im gonna assult you for asking that', 'yes','no','mmmmmaybe','idk bro im packgod']
+        await ctx.send(random.choice(responses))
+    
 # Start the bot using the token from .env or environment variable
 bot.run(os.environ['TOKEN'])
