6b5b845c1b8044bd2782f5c87692955582d82f41
MartTartFart <mtsitrinel@gmail.com>
2025-06-09 17:50:31 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
-{
-  "build": {
-    "env": {}
-  },
-  "deploy": {
-    "startCommand": "bash startup.sh"
-  }
-}
+#!/bin/bash
+
+# Install ffmpeg
+apt-get update
+apt-get install -y ffmpeg
+
+# Start your bot
+python3 main.py
